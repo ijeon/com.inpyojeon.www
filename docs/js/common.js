879ac33a1e5f8a3b9f713757c26a8a690151d0fc
Inpyo Jeon <inpyojeon@hyundaihmall.com>
2023-02-01 19:01:03 +0900

    refac

--- docs/js/common.js
+++ docs/js/common.js
@@ -1,5 +1,13 @@
-$(document).on('click', 'nav button', tabs).on('click', '.gallery button', lBox).on('click', '.dim', lBoxKill);
-$(window).on('scroll', navCheck);
+$(document).on('click', 'nav button', tabs).on('click', '.gallery button', lBox).on('click', '.dim', lBoxKill).on('click', '#clock', toBatcave);
+$(window).on('scroll', navCheck).on('load', init);
+
+var tick;
+
+function init(){
+    getTime();
+    setClock();
+    tick = setInterval(getTime, 1000);
+}
 
 $.fn.activate = function(){
     return this.addClass('active').siblings().removeClass('active');
@@ -29,4 +37,31 @@ function navCheck(){
     var _idx = (_top < _sec1) ? 0 : ((_top >= _sec1 && _top < _sec2) ? 1 : 2);
 
     $('nav button').eq(_idx).activate();
+}
+
+function getTime(){
+    const date = new Date();
+    const hour = date.getHours() % 12;
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+
+    setClock(hour, min, sec);
+}
+
+function setClock(hour, min, sec){
+    var degH = Math.round(360 * (hour / 12)) + min * 0.5 - 180;
+    var degM = Math.round(360 * (min / 60)) - 180;
+    var degS = Math.round(360 * (sec / 60)) - 180;
+
+    $('#hour').css({ transform: 'rotate(' + degH + 'deg)' });
+    $('#min').css({ transform: 'rotate(' + degM + 'deg)' });
+    $('#sec').css({ transform: 'rotate(' + degS + 'deg)' });
+}
+
+function toBatcave(){
+    clearInterval(tick);
+    $('#clock').addClass('set');
+    setTimeout(function(){
+
+    }, 1000);
 }

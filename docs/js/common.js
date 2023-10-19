2363aefa96820dcf588ede3f7b254daf7fc4b0e0
Inpyo Jeon <inpyojeon@hyundaihmall.com>
2023-10-19 15:17:02 +0900

    update

--- docs/js/common.js
+++ docs/js/common.js
@@ -1,101 +1,15 @@
-$(document)
-.on('click', 'nav#gnb button', tabs)
-.on('click', 'nav#tags button', tags)
-.on('click', '.gallery button, .item button, .dim', lBox)
-.on('click', '#clock', toBatcave)
-.on('click', '#top', toTop);
+$(document).on('click', 'imgslide button', lbOpen);
+$(document).on('click', 'dim', lbClose);
 
-$(window)
-.on('scroll', navCheck)
-.on('load', init);
+function lbOpen() {
+    var src = $(this).children('img').attr('src').replace('-thumb', '');
+    window.location.hash = src.split('/').pop().split('.')[0];
 
-var tick;
-
-function init(){
-    if ($('body').hasClass('cave')) {
-        
-    } else {
-        getTime();
-        setClock();
-        tick = setInterval(getTime, 1000);
-    }    
+    $('dim').addClass('active');
+    $('lightbox').css({ backgroundImage: 'url('+src+')' });
 }
 
-$.fn.activate = function(){
-    return this.addClass('active').siblings().removeClass('active');
-}
-
-function tabs(){
-    var idx = $(this).index();
-
-    if (idx < 3) $('html, body').stop().animate({ scrollTop: $('article section').eq(idx).offset().top - $('nav').outerHeight() }, 200);
-    else $('body').toggleClass('eng');
-}
-
-function tags(){
-    var idx = $(this).index();
-
-    $(this).add($('section').eq(idx)).activate();
-    
-}
-
-function lBox(){
-    if ($('.dim').hasClass('active')) $('.dim').empty().removeClass('active');
-    else {
-        var csrc = $(this).find('img').attr('src').replace('-thumb', '');
-
-        $('.dim').append('<img src="' + csrc + '">').addClass('active');
-    }
-}
-
-function navCheck(){
-    var _top = $(window).scrollTop();
-
-    if ($('body').hasClass('cave')) {
-        if (_top > $(window).height()) $('#top').addClass('active');
-        else $('#top').removeClass('active');
-    } else {
-        var _sec1 = $('section').eq(1).offset().top - $('nav#gnb').outerHeight();
-        var _sec2 = $('section').eq(2).offset().top - $('nav#gnb').outerHeight();
-        var _idx = (_top < _sec1) ? 0 : ((_top >= _sec1 && _top < _sec2) ? 1 : 2);
-
-        $('nav#gnb button').eq(_idx).activate();
-    }
-}
-
-function getTime(){
-    const date = new Date();
-    const hour = date.getHours() % 12;
-    const min = date.getMinutes();
-    const sec = date.getSeconds();
-
-    setClock(hour, min, sec);
-}
-
-function setClock(hour, min, sec){
-    var degH = hour * 30 + min * 0.5 - 180;
-    var degM = min * 6 - 180;
-    var degS = sec * 6 - 180;
-
-    $('#hour').css({ transform: 'rotate(' + degH + 'deg)' });
-    $('#min').css({ transform: 'rotate(' + degM + 'deg)' });
-    $('#sec').css({ transform: 'rotate(' + degS + 'deg)' });
-}
-
-function toBatcave(){
-    clearInterval(tick);
-    $('#clock').addClass('set');
-
-    if ($(this).hasClass('hide')) {
-        window.location.href="cave.html";
-    }
-    else {
-        setTimeout(function(){
-            $('#clock').addClass('hide');
-        }, 1300);
-    }    
-}
-
-function toTop(){
-    window.scrollTo(0,0);
+function lbClose() {
+    $('dim').removeClass('active');
+    $('lightbox').css({ backgroundImage: 'none' });
 }
